5d209f438f8230531eaad3eea81a4cb863c873b9
Phạm Quốc Tuấn <tuanphampp9@gmail.com>
2023-01-27 21:27:10 +0700

    refactor code follow routes and app/controllers folders

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const path = require('path')
 const { engine } = require ('express-handlebars');
-
+const route = require('./routes/index')
 const app = express();
+app.use(express.urlencoded({
+    extended:true
+}))//middleware convert data of method post(body) to express(gửi qua form with method post)
 
+app.use(express.json())//gửi with javascript
 app.use(express.static(path.join(__dirname, 'public')))
 app.engine('hbs', engine({extname:'.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-app.get('/home',(req,res)=>{
-    res.render('home')
-})
-app.get('/news',(req,res)=>{
-    res.render('news')
-})
-
+//route init
+route(app)
 app.listen(3000,()=>{
     console.log('server is running')
 })
